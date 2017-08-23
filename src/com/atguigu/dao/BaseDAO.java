6b4726a62dd7f7e7ea8d20c8c4a8aa033e1a3582
Tian <Tian@aiguigu.com>
2017-08-23 11:07:56 +0800

    new

---
+++ src/com/atguigu/dao/BaseDAO.java
@@ -0,0 +1,17 @@
+package com.atguigu.dao;
+
+import java.util.List;
+
+/**
+ * Created by Tian on 2017/8/23.
+ */
+public interface BaseDAO {
+
+    boolean add(Object o);
+
+    boolean delete(Object o);
+
+    boolean update(Object o);
+
+    List find(Object o);
+}
