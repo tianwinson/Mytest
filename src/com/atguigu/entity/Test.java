6cd842f47fc68d9a762e6428881aa2bcb8860bcc
Tian <Tian@aiguigu.com>
2017-08-23 10:30:19 +0800

    Initial commit

---
+++ src/com/atguigu/entity/Test.java
@@ -0,0 +1,82 @@
+package com.atguigu.entity;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+/**
+ * Created by Tian on 2017/8/23.
+ */
+@Entity
+public class Test {
+    private int id;
+    private String author;
+    private Timestamp date;
+    private String title;
+
+    @Id
+    @Column(name = "ID")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "AUTHOR")
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Basic
+    @Column(name = "DATE")
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    @Basic
+    @Column(name = "TITLE")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Test test = (Test) o;
+
+        if (id != test.id) return false;
+        if (author != null ? !author.equals(test.author) : test.author != null) return false;
+        if (date != null ? !date.equals(test.date) : test.date != null) return false;
+        if (title != null ? !title.equals(test.title) : test.title != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        return result;
+    }
+}
